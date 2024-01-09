409873f07e8cd4648986249a5d357ebb01fc3132
Abrham Abayneh <abrshkorra@gmail.com>
2024-01-09 14:09:48 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Add this line
+const cors = require('cors');
 
 const _ = require('lodash');
 
